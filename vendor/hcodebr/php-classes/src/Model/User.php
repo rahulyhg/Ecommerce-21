b8565d0747d7ba0a9e227107e7f66a0145840601
maykowvictor <kiddeveloper5610@gmail.com>
2019-05-01 00:28:27 -0300

    Crud de usuários funcionando .Função recuperar a senha não completa

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -196,7 +196,7 @@ class User extends Model
     {
         $result = base64_decode($result);
         $code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
-        $iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');;
+        $iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');
         $idrecovery = openssl_decrypt($code, 'aes-256-cbc', User::SECRET, 0, $iv);
         $sql = new Sql();
         $results = $sql->select("
