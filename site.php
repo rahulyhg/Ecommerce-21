5e6c6173a7e1d48f373371dc52b14c4ad73a515f
maykow_victor <kiddeveloper5610@gmail.com>
2019-06-12 00:57:00 -0300

    Site - Cart - Session

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->get("/", function() {
 
@@ -44,3 +45,29 @@ $app->get("/categories/:idcategory", function($idcategory) {
 	]);
 
 });
+
+$app->get("/product/:desurl", function($desurl) {
+
+	$product = new Product();
+
+	$product->getFromUrl($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		"product" => $product->getValues(),
+		"categories" => $product->getCategories()
+	]);
+
+});
+
+$app->get("/cart", function() {
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
+});
+
+
